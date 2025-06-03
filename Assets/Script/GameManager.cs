2aca48a07378f41b1771c7f364eee99e756b3047
NguyenVanHoangPhuc <hoangphucnguyenvan1@gmail.com>
2025-06-03 20:41:53 +0700

    Them hien thi thoi gian, game manage, timerUI, thời gian thắng thua, điều kiện qua màn

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int requiredGunsDestroyed = 3;
+    public int requiredTanksDestroyed = 5;
+    public float timeLimit = 120f; // 2 phút
+
+    public GameObject winPanel;
+    public GameObject losePanel;
+
+    private int currentGunsDestroyed = 0;
+    private int currentTanksDestroyed = 0;
+    private float timer;
+
+    private bool gameEnded = false;
+
+    void Start()
+    {
+        timer = timeLimit;
+    }
+
+    void Update()
+    {
+        if (gameEnded) return;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            LoseGame();
+        }
+
+        if (currentGunsDestroyed >= requiredGunsDestroyed &&
+            currentTanksDestroyed >= requiredTanksDestroyed)
+        {
+            WinGame();
+        }
+    }
+
+    public void AddGunDestroyed()
+    {
+        currentGunsDestroyed++;
+    }
+
+    public void AddTankDestroyed()
+    {
+        currentTanksDestroyed++;
+    }
+
+    void WinGame()
+    {
+        gameEnded = true;
+        Debug.Log("WIN GAME!");
+        if (winPanel != null) winPanel.SetActive(true);
+        Invoke("LoadNextLevel", 2f);
+    }
+
+    void LoseGame()
+    {
+        gameEnded = true;
+        Debug.Log("YOU LOSE!");
+        if (losePanel != null) losePanel.SetActive(true);
+        Invoke("ReloadLevel", 2f);
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public float GetTimeRemaining()
+    {
+        return timer;
+    }
+}
