3d8cc795bc36f3c373ef2c3ac48edcd41fe9d035
Pham Thi Thuy Linh <thuylinhcua2024@gmail.com>
2025-06-03 22:35:23 +0700

    Receiving rewards and count (change number of bullets and health component)

--- Assets/Script/Tank/Receiving_Rewards.cs
+++ Assets/Script/Tank/Receiving_Rewards.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class Receiving_Rewards : MonoBehaviour
 {
     public int health = 100;
     public int bullet = 10;
+    public TextMeshProUGUI bulletText;
+    public TextMeshProUGUI healthText;
     void Start()
     {
-        
+        bulletText.SetText(bullet.ToString());
+        healthText.SetText(health.ToString());
+
     }
 
     // Update is called once per frame
@@ -19,11 +24,13 @@ public class Receiving_Rewards : MonoBehaviour
         if (collision.CompareTag("reward_bullet"))
         {
             bullet += 3;
+            bulletText.SetText(bullet.ToString());
             Destroy(collision.gameObject);
         }
         if (collision.CompareTag("reward_health"))
         {
             health += 10;
+            healthText.SetText(health.ToString());
             Destroy(collision.gameObject);
 
         }
