b386a8ad44683dfa54c39c86225b31292870b735
Pham Thi Thuy Linh <thuylinhcua2024@gmail.com>
2025-06-05 14:32:32 +0700

    Modified class Receiving_Rewards : Removed random variables

--- Assets/Script/Tank/Receiving_Rewards.cs
+++ Assets/Script/Tank/Receiving_Rewards.cs
@@ -8,42 +8,28 @@ public class Receiving_Rewards : MonoBehaviour
     public int bullet = 10;
     public TextMeshProUGUI bulletText;
     public TextMeshProUGUI healthText;
-    int hasBullet, hasHealth;
-    
     void Start()
     {
         bulletText.SetText(bullet.ToString());
         healthText.SetText(health.ToString());
-        System.Random random = new System.Random();
-        hasBullet = random.Next(2);
-        hasHealth = random.Next(2);
-        Debug.Log(hasBullet+ " " + hasHealth);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (hasBullet == 1)
-        {
+        
             if (collision.CompareTag("reward_bullet"))
             {
+                Debug.Log("Touched: " + collision.name);
                 bullet += 3;
                 bulletText.SetText(bullet.ToString());
                 Destroy(collision.gameObject);
             }
-        }
-    if (hasHealth == 1)
-        {
+        
             if (collision.CompareTag("reward_health"))
             {
+                Debug.Log("Touched: " + collision.name);
                 health += 10;
                 healthText.SetText(health.ToString());
                 Destroy(collision.gameObject);
             }
-        }
     }
 }
