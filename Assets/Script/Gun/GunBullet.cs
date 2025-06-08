93ec0a122ed1a779ac4f0cda409d4885466a4e8f
Nguyen Dinh Thai Ha <thaiha12051311@gmail.com>
2025-06-08 13:30:42 +0700

    GunWrapper constructor, scripts, health bar

---
+++ Assets/Script/Gun/GunBullet.cs
@@ -0,0 +1,29 @@
+﻿// GunBullet.cs
+using UnityEngine;
+
+public class GunBullet : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 25f;
+    [SerializeField] private float timeDestroy = 5f;
+
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.right * moveSpeed, ForceMode2D.Impulse); // bắn thẳng
+
+        Destroy(gameObject, timeDestroy);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gun"))
+        {
+            GunHealth enemyHealth = collision.GetComponent<GunHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(20); // Gọi phương thức TakeDamage trên đối tượng va chạm
+            }
+        }
+        Destroy(gameObject); // Hủy viên đạn khi va chạm
+    }
+}
