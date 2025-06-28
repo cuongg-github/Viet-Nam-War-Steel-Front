04aabef21e511da582d8836088de74c6327e0380
Nguyen Dinh Thai Ha <thaiha12051311@gmail.com>
2025-06-29 06:13:58 +0700

    map 3

---
+++ Assets/Script/Tank/SkillTank/ShieldSkill.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class ShieldSkill : MonoBehaviour
+{
+    public GameObject shieldVisual; 
+    public float shieldDuration = 15f; 
+    private bool isShieldActive = false;
+    private float shieldTimer = 0f;
+
+    void Start()
+    {
+        if (shieldVisual != null)
+            shieldVisual.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !isShieldActive)
+        {
+            ActivateShield();
+        }
+
+        if (isShieldActive)
+        {
+            shieldTimer -= Time.deltaTime;
+
+            if (shieldTimer <= 0f)
+            {
+                DeactivateShield();
+            }
+        }
+    }
+
+    void ActivateShield()
+    {
+        if (shieldVisual != null)
+            shieldVisual.SetActive(true);
+
+        isShieldActive = true;
+        shieldTimer = shieldDuration;
+
+        // Báo cho tankAI
+        GetComponent<TankAIHealth>().isShieldActive = true;
+    }
+
+    void DeactivateShield()
+    {
+        if (shieldVisual != null)
+            shieldVisual.SetActive(false);
+
+        isShieldActive = false;
+
+        // Tắt bảo vệ
+        GetComponent<TankAIHealth>().isShieldActive = false;
+    }
+}
---
+++ Assets/Script/Tank/TankWayPoint.cs
@@ -0,0 +1,40 @@
+﻿using Pathfinding;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TankWayPoint : MonoBehaviour
+{
+    public Transform[] waypoints;
+    private int currentIndex = 0;
+    private AIDestinationSetter destinationSetter;
+
+    void Start()
+    {
+        destinationSetter = GetComponent<AIDestinationSetter>();
+        if (waypoints.Length > 0)
+        {
+            destinationSetter.target = waypoints[0];
+        }
+    }
+
+    void Update()
+    {
+        if (waypoints.Length == 0 || destinationSetter.target == null)
+            return;
+
+        float dist = Vector2.Distance(transform.position, destinationSetter.target.position);
+        if (dist < 0.5f)
+        {
+            currentIndex++;
+            if (currentIndex < waypoints.Length)
+            {
+                destinationSetter.target = waypoints[currentIndex];
+            }
+            else
+            {
+                destinationSetter.target = null; // Dừng lại
+            }
+        }
+    }
+}
